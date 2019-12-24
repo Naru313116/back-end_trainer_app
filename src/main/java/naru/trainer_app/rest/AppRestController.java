8ffc032a2e888b4fb2c491b5ecd4ac8c24ddb84d
Rafał <omega313116@gmail.com>
2019-12-24 22:48:03 +0100

    deploy first rest mapping for test

--- src/main/java/naru/trainer_app/rest/AppRestController.java
+++ src/main/java/naru/trainer_app/rest/AppRestController.java
@@ -1,16 +1,29 @@
 package naru.trainer_app.rest;
 
 
+import naru.trainer_app.model.Trainer;
+import naru.trainer_app.service.TrainerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("app")
 public class AppRestController {
 
-    @GetMapping("/test")
-    String hello(){
-        return "hello world";
+    private final TrainerService trainerService;
+
+    public AppRestController(TrainerService trainerService) {
+        this.trainerService = trainerService;
     }
+
+    @GetMapping("/trainers")
+    List<Trainer> getTrainerList(){
+        return trainerService.findAll();
+    }
+
+
 }
